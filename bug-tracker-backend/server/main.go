17815849ad3ddfa8d226cf10051a0449fbea0e4b
Trinity <trinity@Trinitys-MacBook-Air.local>
2025-09-28 19:20:08 -0500

    updated config

--- bug-tracker-backend/server/main.go
+++ bug-tracker-backend/server/main.go
@@ -20,7 +20,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5000","https://bug-tracker-wk6k.vercel.app/api/v1"},
+		AllowOrigins:     []string{"http://localhost:5000","https://bug-tracker-wk6k.vercel.app","https://bug-tracker-hvx2.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
